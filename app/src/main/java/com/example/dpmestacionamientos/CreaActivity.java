bd1f064753a3799e89608af702bce8d4c21193c2
bryant <tello@bryants-MacBook-Pro.local>
2019-08-25 15:21:38 -0500

    segundo commit

---
+++ app/src/main/java/com/example/dpmestacionamientos/CreaActivity.java
@@ -0,0 +1,14 @@
+package com.example.dpmestacionamientos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crea);
+    }
+}
---
+++ app/src/main/java/com/example/dpmestacionamientos/Edit_Profile_Activity.java
@@ -0,0 +1,14 @@
+package com.example.dpmestacionamientos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Edit_Profile_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit__profile_);
+    }
+}
