3d37d94f6c4c697dc2756be0ca30ad32d078bec7
Edgar Huarcaya Chocce <ehuarcaya@teon.pe>
2020-04-17 19:24:55 -0500

    Comentar opciones que no van en sprint 1

--- app/src/main/java/com/example/estacionaaqui/CreaActivity.java
+++ app/src/main/java/com/example/estacionaaqui/CreaActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
@@ -20,29 +21,32 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CreaActivity extends AppCompatActivity {
 
     EditText nombre, apellido, correo, contra;
-    RadioButton tipo1, tipo2;
+    CheckBox tipo1, tipo2;
     FirebaseDatabase database;
     DatabaseReference myRef;
 
     FirebaseAuth mAuth;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crea);
 
         nombre = findViewById(R.id.etName);
-        apellido= findViewById(R.id.etapellido);
-        correo= findViewById(R.id.etCorreo);
-        contra= findViewById(R.id.etContraseña);
-        tipo1 =findViewById(R.id.rbDueño);
-        tipo2 =findViewById(R.id.rbCliente);
+        apellido = findViewById(R.id.etapellido);
+        correo = findViewById(R.id.etCorreo);
+        contra = findViewById(R.id.etContraseña);
+        tipo1 = findViewById(R.id.ckbDueno);
+        tipo2 = findViewById(R.id.ckbCliente);
 
-
-    iniciofirebase();
+        iniciofirebase();
 
         mAuth = FirebaseAuth.getInstance();
     }
@@ -53,82 +57,77 @@ public class CreaActivity extends AppCompatActivity {
         myRef = database.getReference();
     }
 
-    public void Crear_New(View view)
-    {
+    public void Crear_New(View view) {
         final String nom = nombre.getText().toString();
         final String Ape = apellido.getText().toString();
-        final String cor=correo.getText().toString();
+        final String cor = correo.getText().toString();
         final String contrase = contra.getText().toString();
         final boolean t1 = tipo1.isChecked();
         final boolean t2 = tipo2.isChecked();
-        if(nom.equals(""))
-        {
+        if (nom.equals("")) {
             validacion();
 
-        }
-      else  if (Ape.equals("")){
+        } else if (Ape.equals("")) {
             validacion();
 
-        }
-       else if (cor.equals("")){
+        } else if (cor.equals("")) {
             validacion();
-        }
-       else if (contrase.equals("")){
+        } else if (contrase.equals("")) {
             validacion();
-        }
-      else  if(t1==false && t2 ==false)
-        {
-            Toast.makeText(this,"Falta Tipo de Usuario",Toast.LENGTH_LONG).show();
-        }
-      else  if(t1==true && t2==true){
-            Toast.makeText(this,"Elija solo un Tipo de Usuario",Toast.LENGTH_LONG).show();
+        } else if (t1 == false && t2 == false) {
+            Toast.makeText(this, "Por favor, seleccione el tipo de usuario", Toast.LENGTH_LONG).show();
+        } else {
 
-        }
-        else {
-
-    mAuth.createUserWithEmailAndPassword(cor,contrase).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-        @Override
-        public void onComplete(@NonNull Task<AuthResult> task) {
-            if(task.isSuccessful())
-            {
-                String id = mAuth.getCurrentUser().getUid();
-                Map<String,Object> map = new HashMap<>();
-
-                map.put("Nombre",nom);
-                map.put("Correo",cor);
-                map.put("Apellido",Ape);
-                map.put("Contrasenia",contrase);
-                if(t1)
-                {
-                    map.put("tipo","due");
-                }
-                if(t2)
-                {
-                    map.put("tipo","cli");
-                }
-                myRef.child("persona").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+            if (!cor.trim().matches(emailPattern)) {
+                Toast.makeText(CreaActivity.this, "Por favor, ingrese un correo válido ", Toast.LENGTH_SHORT).show();
+            } else if (contrase.length() < 6 || !isValidPassword(contrase)) {
+                Toast.makeText(CreaActivity.this, "Por favor, ingrese una contraseña válida ", Toast.LENGTH_SHORT).show();
+            } else {
+                mAuth.createUserWithEmailAndPassword(cor, contrase).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onComplete(@NonNull Task<Void> task2) {
-                        if(task2.isSuccessful()){
-                            if(t1==true){
-                                startActivity(new Intent(CreaActivity.this,DuenoActivity.class));
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            String id = mAuth.getCurrentUser().getUid();
+                            Map<String, Object> map = new HashMap<>();
+
+                            map.put("Nombre", nom);
+                            map.put("Correo", cor);
+                            map.put("Apellido", Ape);
+                            map.put("Contrasenia", contrase);
+
+                            if (t1 == true && t2 == true) {
+                                map.put("tipo", "cd");
+                            } else {
+                                if (t1 == true) {
+                                    map.put("tipo", "due");
+                                }
+                                if (t2 == true) {
+                                    map.put("tipo", "cli");
+                                }
                             }
-                            if(t2==true) {
-                                startActivity(new Intent(CreaActivity.this,ClienteActivity.class));
-                            }
-                        }
-                        else
-                        {
-                            Toast.makeText(CreaActivity.this, "No se creo los datos ", Toast.LENGTH_SHORT).show();
+
+                            myRef.child("persona").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task2) {
+                                    if (task2.isSuccessful()) {
+                                        if (t1 == true) {
+                                            startActivity(new Intent(CreaActivity.this, DuenoActivity.class));
+                                        }
+                                        if (t2 == true) {
+                                            startActivity(new Intent(CreaActivity.this, ClienteActivity.class));
+                                        }
+                                    } else {
+                                        Toast.makeText(CreaActivity.this, "No se creo los datos ", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        } else {
+                            Toast.makeText(CreaActivity.this, "No se pudo registrar este usuario ", Toast.LENGTH_SHORT).show();
+
                         }
                     }
                 });
             }
-            else{ Toast.makeText(CreaActivity.this, "No se pudo registrar este usuario ", Toast.LENGTH_SHORT).show();
-
-            }
-        }
-    });
 
         }
     }
@@ -136,25 +135,36 @@ public class CreaActivity extends AppCompatActivity {
     private void validacion() {
         String nom = nombre.getText().toString();
         String Ape = apellido.getText().toString();
-        String cor=correo.getText().toString();
+        String cor = correo.getText().toString();
         String contrase = contra.getText().toString();
 
 
-        if (nom.equals("")){
-            nombre.setError("Required");
+        if (nom.equals("")) {
+            nombre.setError("Por favor, ingrese su nombre");
         }
-        if (Ape.equals("")){
-            apellido.setError("Required");
+        if (Ape.equals("")) {
+            apellido.setError("Por favor, ingrese su apellido");
         }
-        if (cor.equals("")){
-            correo.setError("Required");
+        if (cor.equals("")) {
+            correo.setError("Por favor, ingrese su correo");
         }
-        if (contrase.equals("")&& contrase.length()>6){
-            contra.setError("Required");
+        if (contrase.equals("")) {
+            contra.setError("Por favor, ingrese una contraseña");
         }
 
     }
 
+    public static boolean isValidPassword(final String password) {
+
+        Pattern pattern;
+        Matcher matcher;
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+
+        return matcher.matches();
+
+    }
 
 
 }
